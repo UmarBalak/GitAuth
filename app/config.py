5a8f119657ecebe245126419ab6a4830ce42751a
Umar Balak <umarbalak35@gmail.com>
2025-02-21 16:43:08 +0530

    fix redirect issue

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     GITHUB_CLIENT_ID: str
     GITHUB_CLIENT_SECRET: str
-    GITHUB_REDIRECT_URI: str = "https://gitauth-iza6.onrender.com/login"
+    GITHUB_REDIRECT_URI: str = "https://gitauth-iza6.onrender.com/callback"
     APP_HOST: str = "https://ai-2namqliw7-ubaid-khans-projects-c47937ab.vercel.app"
 
     class Config:
